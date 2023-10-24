eb90c686e04f5f6b7c83f89a0b6885529ee50766
HatCherry <kpospiech.tg@gmail.com>
2023-10-24 22:16:47 +0200

    Updated HW1_2_1.py

--- HW1_2/HW1_2_1.py
+++ HW1_2/HW1_2_1.py
@@ -1,11 +1,12 @@
 import sympy as sp
+import matplotlib.pyplot as plt
 
 # User Parameters (Input Section)
 q_val = 1.0  # Dimensionless source strength
 dt = 4.0E-4  # Time step
 x_start, y_start = -2, -10**-6  # Initial point
 n_steps = 26000  # Number of integration steps
-x_stop = 2.0  # Stopping x value
+x_stop = 2  # Stopping value for x
 
 # Define the symbolic variables
 x_prime, y_prime, q_prime = sp.symbols('x_prime y_prime q_prime')
@@ -34,7 +35,7 @@ path = [(x_start, y_start)]
 V_prime_values = []
 Cp_values = []
 
-for _ in range(n_steps):
+for i in range(n_steps):
     x_current, y_current = path[-1]
     
     # Compute velocities and modulus
@@ -47,7 +48,6 @@ for _ in range(n_steps):
     V_prime_values.append(float(V_prime))
     Cp_values.append(float(Cp))
     
-    # Check the stop condition
     if x_current >= x_stop:
         break
     
@@ -55,8 +55,18 @@ for _ in range(n_steps):
     next_x, next_y = euler_step(x_current, y_current, dt, q_val)
     path.append((next_x, next_y))
 
-
 # Print the results
-for i in range(len(V_prime_values)):
-    x, y = path[i]
-    print(f"Step {i+1}: Coordinate (x', y') = ({x:.5f}, {y:.5f}), Velocity Modulus V' = {V_prime_values[i]:.5f}, Pressure Coefficient Cp = {Cp_values[i]:.5f}")
+for i, (x, y) in enumerate(path):
+    if i < len(V_prime_values):
+        print(f"Step {i+1}: Coordinate (x', y') = ({x:.5f}, {y:.5f}), Velocity Modulus V' = {V_prime_values[i]:.5f}, Pressure Coefficient Cp = {Cp_values[i]:.5f}")
+
+# Plotting the streamline
+x_values = [point[0] for point in path]
+y_values = [point[1] for point in path]
+
+plt.plot(x_values, y_values, '-b')
+plt.xlabel("x'")
+plt.ylabel("y'")
+plt.title("Streamline Plot")
+plt.grid(True)
+plt.show()
