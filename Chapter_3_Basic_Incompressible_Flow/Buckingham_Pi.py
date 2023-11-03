65f99ac87b1f68a6d7ef7e1d25d2af6eaf25f0a0
HatCherry <kpospiech.tg@gmail.com>
2023-11-03 22:51:15 +0100

    Updated Buckingham_Pi.py

--- Chapter_3_Basic_Incompressible_Flow/Buckingham_Pi.py
+++ Chapter_3_Basic_Incompressible_Flow/Buckingham_Pi.py
@@ -1,51 +1,59 @@
-from sympy import symbols, Eq, solve
+from sympy import symbols, solve, Eq
 
-def parse_dimensions(dim_str):
-    """Parses dimension string into a dictionary."""
-    dimensions = {}
-    i = 0
-    while i < len(dim_str):
-        if dim_str[i] in ['^', '-']:
-            power = int(dim_str[i+1])
-            if dim_str[i] == '-':
-                power = -power
-            dimensions[dim_str[i-1]] = power
-            i += 2
-        else:
-            dimensions[dim_str[i]] = 1
-            i += 1
-    return dimensions
+def get_dimensions(var):
+    return variables[var]
 
-def buckingham_pi(variables, basic_dims):
-    pi_terms = []
+def form_dimensionless_product(target, repeating_vars):
+    a, b, c = symbols('a b c')
+    target_dims = get_dimensions(target)
 
-    # List of symbols
-    coeffs = symbols('a0:' + str(len(variables)))
-    equations = []
+    # Creating the dimensional equation for the product
+    product_dims = {
+        'M': 0,
+        'L': 0,
+        'T': 0
+    }
 
-    for dim in basic_dims:
-        equation = sum([coeffs[j] * parse_dimensions(dim_val).get(dim, 0) for j, dim_val in enumerate(variables.values())])
-        equations.append(equation)
+    symbol_to_var = {}  # To map back symbols to variable names
+    for var, symbol in repeating_vars.items():
+        dims = get_dimensions(var)
+        for dim, value in dims.items():
+            product_dims[dim] += value * symbol
+        symbol_to_var[symbol] = var
 
-    solutions = solve(equations, coeffs)
+    # Setting up the equations based on the powers of dimensions
+    eqns = []
+    for dim, power in target_dims.items():
+        eqns.append(Eq(product_dims[dim] + power, 0))
 
-    # Generate the Pi terms
-    for i, coeff in enumerate(coeffs):
-        pi_term = f"Pi_{i+1} = {variables.keys()[i]}^{solutions[coeff]}"
-        pi_terms.append(pi_term)
+    sol = solve(eqns, (a, b, c))
+    
+    # Constructing the Pi term
+    pi_term = f"{target} / ("
+    for symbol, power in sol.items():
+        var = symbol_to_var[symbol]
+        pi_term += f"{var}^{power} * "
+    pi_term = pi_term.rstrip(" * ") + ")"
 
-    return pi_terms
+    return pi_term
 
 if __name__ == "__main__":
+    # Example usage
     variables = {
-        'V': 'LT^-1',
-        'L': 'L',
-        'rho': 'ML^-3',
-        'mu': 'ML^-1T^-1',
-        'g': 'LT^-2'
+        'V': {'L': 1, 'T': -1},
+        'L': {'L': 1},
+        'rho': {'M': 1, 'L': -3},
+        'mu': {'M': 1, 'L': -1, 'T': -1},
+        'g': {'L': 1, 'T': -2}
     }
-    dimensions = ['M', 'L', 'T']
+    
+    # Choosing three repeating variables (as an example)
+    repeating = {
+        'V': symbols('a'),
+        'L': symbols('b'),
+        'rho': symbols('c')
+    }
 
-    pi_groups = buckingham_pi(variables, dimensions)
-    for group in pi_groups:
-        print(group)
+    # Find dimensionless products for the rest of the variables
+    for var in set(variables.keys()) - set(repeating.keys()):
+        print(form_dimensionless_product(var, repeating))
