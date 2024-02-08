6eee41b3e8d960aaa5468e842edaf14dc031f889
HatCherry <kpospiech.tg@gmail.com>
2024-02-08 20:10:58 +0100

    Updated HW5.py

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -23,11 +23,11 @@ initial_guess = 0.02
 # Solve for lambda_DW
 lambda_DW_solution = fsolve(colebrook, initial_guess)
 
-# Calculate friction velocity u*
-u_star = np.sqrt(lambda_DW_solution * (U0**2) / (2 * R))
-
 # Calculate the axial pressure gradient
 dp_dx = lambda_DW_solution * (rho * U0**2) / (2 * R)
+
+# Calculate friction velocity u*
+u_star = np.sqrt(dp_dx * R / (4 * rho))
 
 # Display the results
 print(Re_D, lambda_DW_solution[0], u_star[0], dp_dx[0], nu)
